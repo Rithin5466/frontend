ed5cb7101e18c614d6b812f960bca46fcd2fe029
Rithin5466 <chegondirithinsurya@gmail.com>
2024-08-11 17:17:50 +0530

    no chnages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { SignIn } from './components/signin/signin';
 import { SignUp } from './components/signup/signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ForgotPassword } from './components/updatePassword/forgetpass';
+import YogaFestival from './components/landing pages/landpage1';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-
+        {/* <Route path="/home" element={<YogaFestival/>}/> */}
       </Routes>
     </BrowserRouter>
   );
